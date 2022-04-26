c06a08d63c24709fca2dc296ba9cab2ec225f505
kiparisa91 <kiparisa91@gmail.com>
2022-04-26 22:51:44 +0500

    Дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 	// write your code here
+    //Задача1
+    String firstName = "Ivan";
+    String middleName = "Ivanovich";
+    String lastName = "Ivanov";
+    String fullName = lastName + " " + firstName + " " + middleName;
+    System.out.println("ФИО сотрудника - " + fullName);
+
+
+    //Задача2
+    String fullName1 = "Ivanov Ivan Ivanovich";
+    String fullName2 = fullName.toUpperCase();
+    System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName2);
+
+
+    //Задача3
+    String fullName3 = "Иванов Семён Семёнович";
+    String fullNameReplace = fullName3.replace('ё', 'е');
+    System.out.println("Данные ФИО сотрудника - " + fullNameReplace);
+
+
     }
 }
